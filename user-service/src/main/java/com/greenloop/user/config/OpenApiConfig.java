99a3d0c24fd0ea97493ce33d30ab3b755bdceefe
thanhnt-dev <tanthanh.060918@gmail.com>
2025-10-24 22:54:18 +0700

    Update Swagger Config

--- user-service/src/main/java/com/greenloop/user/config/OpenApiConfig.java
+++ user-service/src/main/java/com/greenloop/user/config/OpenApiConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,10 +15,31 @@ import java.util.List;
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${GATEWAY_HOST}")
+    private String gatewayHost;
+
+    @Value("${SERVICE_HOST}")
+    private String serviceHost;
+
+    @Value("${server.port}")
+    private String servicePort;
+
+    @Value("${spring.application.name}")
+    private String serviceName;
+
     @Bean
     public OpenAPI customOpenAPI() {
+        Server gatewayServer = new Server();
+        gatewayServer.setUrl("http://" + gatewayHost + ":8080/" + serviceName);
+        gatewayServer.setDescription("Gateway (Production)");
+
+        Server directServer = new Server();
+        directServer.setUrl("http://" + serviceHost + ":" + servicePort);
+        directServer.setDescription("Direct (Internal)");
+
         return new OpenAPI()
-                .info(new Info().title("GreenLoop User Service API").version("1.0.0"))
+                .info(new Info().title("GreenLoop " + serviceName + " API").version("1.0.0"))
+                .servers(List.of(gatewayServer, directServer))
                 .components(
                         new Components()
                                 .addSecuritySchemes(
@@ -27,5 +50,4 @@ public class OpenApiConfig {
                                                 .bearerFormat("JWT")))
                 .security(List.of(new SecurityRequirement().addList("bearerAuth")));
     }
-
 }
