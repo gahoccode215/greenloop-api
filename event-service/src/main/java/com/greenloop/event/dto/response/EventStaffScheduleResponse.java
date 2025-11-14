7de6e54a1363c45181776d266fc64494f30ccec7
thanhnt-dev <tanthanh.060918@gmail.com>
2025-11-14 19:45:07 +0700

    Update API Category For Donation

--- event-service/src/main/java/com/greenloop/event/dto/response/EventStaffScheduleResponse.java
+++ event-service/src/main/java/com/greenloop/event/dto/response/EventStaffScheduleResponse.java
@@ -1,31 +1,29 @@
 package com.greenloop.event.dto.response;
 
 import com.greenloop.event.enums.EventStatus;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Builder
 public class EventStaffScheduleResponse {
-    private Long staffId;
-    private String fullName;
-    private String email;
-    private Boolean isStoreManager;
-    private Long eventId;
-    private String code;
-    private String name;
-    private String location;
-    private String imageUrl;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
-    private EventStatus status;
-    private String latitude;
-    private String longitude;
-
+  private Long staffId;
+  private String fullName;
+  private String email;
+  private Boolean isStoreManager;
+  private Long eventId;
+  private String code;
+  private String name;
+  private String location;
+  private String imageUrl;
+  private LocalDateTime startTime;
+  private LocalDateTime endTime;
+  private EventStatus status;
+  private String latitude;
+  private String longitude;
 }
